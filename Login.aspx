0f9ee6f0aa0c55a1f829ca7b2f53ddfdaca2ca24
Smeet Parmar <smeetparmar2001@gmail.com>
2022-02-17 12:40:40 +0530

    LOGIN

---
+++ Login.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="Login" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+</head>
+<body>
+<table align="center" style="height: 153px; width: 450px">
+<tr><td>
+   <form id="form1" runat="server">
+   <div class="col-md-6">
+    <asp:Label ID="Label1" runat="server" Text="Login Form" Font-Bold="True" 
+            Font-Italic="True" Font-Size="XX-Large"></asp:Label></tr></td>
+  <tr><td><div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label"><br/> Username</label>
+     <asp:TextBox ID="TextBox1" type="text" class="form-control" runat="server"></asp:TextBox>
+  </div></tr></td>
+  <tr><td><div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <asp:TextBox ID="TextBox2" type="password" class="form-control" runat="server"></asp:TextBox>
+  </div></tr></td>
+  <tr><td> <asp:Button ID="Button1" type="submit" class="btn btn-primary" 
+          runat="server" Text="Login" onclick="demo" />
+    </div>
+    </form></tr></td>
+    </table>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+public partial class Login : System.Web.UI.Page
+{
+    SqlConnection c;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        c = new SqlConnection(ConfigurationManager.ConnectionStrings["DatabaseConnectionString1"].ConnectionString);
+    }
+    protected void demo(object sender, EventArgs e)
+    {
+
+        try
+        {
+            SqlCommand s = new SqlCommand("SELECT COUNT(*) FROM [user] WHERE [name]=@n AND [password]=@p", c);
+            s.Parameters.AddWithValue("@n", TextBox1.Text.Trim());
+            s.Parameters.AddWithValue("@p", TextBox2.Text.Trim());
+            c.Open();
+            int a = (int)s.ExecuteScalar();
+            if (a == 1)
+            {
+                Session["name"] = TextBox1.Text;
+                TextBox1.Text = "";
+                TextBox2.Text = "";
+                Response.Redirect("~/Default.aspx");
+            }
+            else
+            {
+                TextBox1.Text = "";
+                TextBox2.Text = "";
+                Response.Write("<script>alert('Invalid Credentials............')</script>");
+            }
+            c.Close();
+        }
+        catch (SqlException ex)
+        {
+            Response.Write("<script>alert('" + ex.ToString() + "')</script>");
+        }
+    }
+}
